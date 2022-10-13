1e0f045f9bac386ed78553a0cbb1c1c742429e8c
Jack Graham <jack.blair.graham@gmail.com>
2022-10-13 14:45:53 -0500

    convert numbers to letter grades

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,6 @@
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
         //For all of the following exercises, create a new class named ControlFlowExercises with a main method.
@@ -49,20 +52,89 @@ public class ControlFlowExercises {
 //        For the multiples of five: print “Buzz”.
 //        For numbers which are multiples of both three and five: print “FizzBuzz”.
 
-        for (int i = 0; i < 100; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0)) {
-                System.out.println("FizzBuzz");
-            }
-            else if (i % 3 == 0){
-                    System.out.println("Fizz");
-            }
-            else if(i % 5 == 0){
-                    System.out.println("Buzz");
-            }
-            else {
-                    System.out.println(i);
-                    }
-                }
+//        for (int i = 0; i < 100; i++) {
+//            if ((i % 3 == 0) && (i % 5 == 0)) {
+//                System.out.println("FizzBuzz");
+//            }
+//            else if (i % 3 == 0){
+//                    System.out.println("Fizz");
+//            }
+//            else if(i % 5 == 0){
+//                    System.out.println("Buzz");
+//            }
+//            else {
+//                    System.out.println(i);
+//                    }
+//                }
+/////Section 3//////Display Table of Powers//////////////////
+
+        //formatter and scanner utils imported at top of document
+
+        //example array below
+        //int num[] = {10, 21, 13, 4, 15, 6, 27, 8, 19};
+
+//        Formatter fmt = new Formatter();
+//        Scanner scanner = new Scanner(System.in);
+////        int num[] = {1};
+//        String userChoice;
+//            do {
+//                System.out.println("Enter an integer:");
+//                int userInput = scanner.nextInt();
+//                System.out.println("Here's your table");
+//                System.out.println("");
+//                System.out.println("number | squared | cubed");
+//                System.out.println("------ | ------- | ------");
+//                for (int i = 1; i <= userInput; i++) {
+//                    System.out.printf("%-7d| %-8d| %-7d%n", i, i * i, i * i * i);
+//                }
+//                System.out.println("continue? (y/n)");
+//                userChoice = scanner.next();
+//            } while (userChoice.equals("y"));
+
+//below is a frankensteined mess
+//        //table generator for loop goes here
+//
+//        fmt.format("%15s %15s %15s\n", "Number", "Square", "Cube");
+//        for (int n : num)
+//        {
+//            fmt.format("%14s %14s %17s\n", n, (n*n), (n*n*n));
+//        }
+//        System.out.println(fmt);
+//
+//
+//        System.out.println("Would you like to enter a new integer for the table?");
+//        System.out.println("y/n?");
+//        // add an if statement here
+//        // y = enter your number, then add number to the num[] array
+//        // n = print table and break
+//
+//        String yn = scanner.nextLine();
+//        if (yn == "y"){
+//            System.out.println("Enter an integer: ");
+//            //num[i] = input.nextInt();
+//            String skipLine = scanner.nextLine();
+//        }
+/////Section 4//////Convert given number grades into letter grades//////////////////
+
+        System.out.println("Enter a numerical grade from 0 to 100:");
+        Scanner scanner = new Scanner(System.in);
+        byte grade = scanner.nextByte();
+        char letterGrade;
+        if (grade >= 0 && grade <= 59){
+            letterGrade = 'F';
+        } else if (grade > 59 && grade <= 66){
+            letterGrade = 'D';
+        } else if (grade > 66 && grade <= 79){
+            letterGrade = 'C';
+        } else if (grade > 79 && grade <= 87){
+            letterGrade = 'B';
+        } else {
+            letterGrade = 'A';
+        }
+        System.out.println("The letter grade is " + letterGrade);
+
+
+
 
 
     }
