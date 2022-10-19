d9b8b0a3c34a828dc1da1783e1913350bbd5a604
Jack Graham <jack.blair.graham@gmail.com>
2022-10-19 10:16:59 -0500

    java exercises

---
+++ src/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    private String name;
+
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    //setter, name is a property of the person class
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.printf("%s says hello", name);
+    }
+
+    //"no args" default constructor, this exists without you making it
+    public Person(){}
+
+    //overloaded constructor, this overrides the default constructor
+    //"custom default"
+    //"explicit default"
+    public Person(String name) {
+        this.name = name;
+    }
+}
---
+++ src/rpg/RPG.java
@@ -0,0 +1,10 @@
+package rpg;
+
+import java.util.Scanner;
+
+public class RPG {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner()
+    }
+}
