93785c147e05ebe55ef07dfec3e4ea70ce302314
Jack Graham <jack.blair.graham@gmail.com>
2022-10-26 13:57:26 -0500

    input lesson

---
+++ src/root/java/TernaryTester.java
@@ -0,0 +1,10 @@
+package root.java;
+
+
+public class TernaryTester {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7);
+    }
+}
+//I have no idea what's happening here
